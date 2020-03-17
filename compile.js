175b1550267809390244ff397d92d4aa15e676fd
rachit2501 <rac.sri25@gmail.com>
2020-03-17 16:34:21 +0530

    contract

--- compile.js
+++ compile.js
@@ -5,4 +5,4 @@ const solc = require('solc');
 const inboxPath = path.resolve(__dirname,'contracts','inbox.sol');
 
 const source = fs.readFileSync(inboxPath , 'utf8');
-solc.compile(source,1)
+console.log(solc.compile(source,1))
